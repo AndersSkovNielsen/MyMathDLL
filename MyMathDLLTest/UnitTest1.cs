938bdb801efefa9d7a1cf13e69dcc9d8a3172cd7
AndersSkovNielsen <anderssnielsen@live.dk>
2018-09-17 09:55:37 +0200

    Add project files.

---
+++ MyMathDLLTest/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyMathDLL;
+
+namespace MyMathDLLTest
+{
+    [TestClass]
+    public class MathTest
+    {
+        MyMath m = new MyMath();
+
+        [TestMethod]
+        public void AddTest()
+        {
+            //== er ok til simple typer (5 er 5, true er true, 4 er ikke 5)
+            //.Equals er nødvendig til objekter (PersonA er PersonA ifølge hvad? PersonNr?)
+            Assert.IsTrue(m.Add(2, 3).Equals(5));
+        }
+        [TestMethod]
+        public void AddTestNegativeNumber()
+        {
+            Assert.AreEqual(m.Add(-3, -4), -7);
+        }
+    }
+}
